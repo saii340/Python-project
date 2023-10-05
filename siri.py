fc9702fac223ccb52716b87ec85e2dd84fc050b8
saii340 <111050937+saii340@users.noreply.github.com>
2023-10-05 12:33:00 +0530

    Update siri.py

--- siri.py
+++ siri.py
@@ -1,1 +1,41 @@
-print("Hi")
+from flask import Flask, request, render_template
+
+ 
+
+app = Flask(__name__)
+
+ 
+
+@app.route('/')
+
+def index():
+
+    return render_template('index.html')
+
+ 
+
+@app.route('/process', methods=['POST'])
+
+def process():
+
+    input_data = request.form['input_field']
+
+    processed_output = process_input(input_data)
+
+    return render_template('result.html', output=processed_output)
+
+ 
+
+def process_input(input_data):
+
+    # Your processing logic here
+
+    # Return the processed output
+
+    return input_data.upper()
+
+ 
+
+if __name__ == '__main__':
+
+    app.run(host='0.0.0.0', port=80)
